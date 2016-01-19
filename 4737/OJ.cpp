a3866018c48ba42d96c17b9d717f01cded1f8715
ponderway <baodaxiang1991@163.com>
2016-01-19 15:14:16 +0800

    删除重复字符串

---
+++ 4737/OJ.cpp
@@ -0,0 +1,38 @@
+﻿
+#include "OJ.h"
+#include<iostream>
+/*
+Description  
+         给定一个字符串，将字符串中所有和前面重复多余的字符删除，其余字符保留，输出处理后的字符串。需要保证字符出现的先后顺序。
+Prototype
+         int GetResult(const char *input, char *output)
+Input Param 
+         input     输入的字符串
+Output Param 
+         output    输出的字符串
+Return Value
+         0         成功
+         -1        失败及异常
+*/
+int GetResult(const char *input, char *output)
+{	
+	if(input == NULL || output == NULL)
+		return -1;
+		
+	const int NUM = 1000;
+	int flags[NUM] = { 0 };
+
+	while (*input != '\0')
+	{
+		if (flags[*input] == 0)
+		{
+			flags[*input] = 1;
+			*output++ = *input;
+		}
+		++input;
+	}
+	*output = '\0';
+	
+    return 0;
+	
+}
---
+++ 4737/OJ.h
@@ -0,0 +1,7 @@
+#ifndef __OJ_H__
+#define __OJ_H__
+
+int GetResult(const char *input, char *output);
+
+
+#endif
