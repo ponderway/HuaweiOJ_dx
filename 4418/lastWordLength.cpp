42e7aa4ea6bb2742e3834a088b3be370f6c00e06
ponderway <baodaxiang1991@163.com>
2016-01-20 10:13:45 +0800

    字符串最后一个单词的长度

---
+++ 4418/lastWordLength.cpp
@@ -0,0 +1,20 @@
+﻿// 字符串最后一个单词的长度
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	getline(cin, s);
+
+	int len = s.length();
+	int i = len - 1;
+	int N = 0; // 最后一个单词长度
+	while (i >= 0 && s[i] != ' ')
+	{
+		--i;
+		++N;
+	}
+	cout << N << endl;
+}
